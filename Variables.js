66ded86ccd290c493e37322c2e229221cfac18df
Web864 <thelogic266@gmail.com>
2026-05-04 07:42:37 +0000

    Learned Variables

---
+++ Variables.js
@@ -0,0 +1,9 @@
+const accountId = 14482738
+let accountEmail = "jsyuey@gmail.com"
+var accountPassword = "12345"
+// accountCity = "sjkdjshjb"
+let accountState;
+// accoutnId = 2
+
+// console.log(accountId);
+console.table([accountId, accountEmail,accountPassword, accountState])
