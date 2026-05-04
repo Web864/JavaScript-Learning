69adf33a92b98d6bb5ca18a9871a16388d87bab5
Web864 <thelogic266@gmail.com>
2026-05-04 08:06:48 +0000

    conversionOperations

---
+++ conversionOperation.js
@@ -0,0 +1,13 @@
+let score = null
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let isLoggedIn = 1
+ let boleanIsLoggedIn = Boolean(isLoggedIn)
+
+ console.log(boleanIsLoggedIn);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber);
