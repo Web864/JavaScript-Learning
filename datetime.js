ac0541d611321fb4a4dee20c84b92005de2645d5
Web864 <thelogic266@gmail.com>
2026-05-05 07:16:39 +0000

    Date & time Learned

---
+++ datetime.js
@@ -0,0 +1,26 @@
+
+let mydate = new Date()
+
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toISOString());
+// console.log(mydate.toLocaleString());
+// console.log(mydate.toLocaleDateString());
+// console.log(mydate.toLocaleTimeString());
+
+
+
+console.log(typeof mydate);
+
+// let myCreateDAte = new Date(2023, 0 , 29);
+let myCreateDAte = new Date(2023, 0 , 29, 5,3);
+
+
+// console.log(myCreateDAte.toLocaleString());
+
+
+let myTimeStamp = Date.now()
+
+console.log(myTimeStamp);
+
+console.log(myCreateDAte.getTime());
