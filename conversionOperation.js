1a7a9ae4c1956ac26d851df989a2bc5130d5d945
Web864 <thelogic266@gmail.com>
2026-05-05 06:57:29 +0000

    conversionoperations

--- conversionOperation.js
+++ conversionOperation.js
@@ -1,13 +1,43 @@
-let score = null
+// let score = null
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
-let isLoggedIn = 1
- let boleanIsLoggedIn = Boolean(isLoggedIn)
+// let isLoggedIn = 1
+//  let boleanIsLoggedIn = Boolean(isLoggedIn)
 
- console.log(boleanIsLoggedIn);
-// let valueInNumber = Number(score)
-// console.log(typeof valueInNumber);
+//  console.log(boleanIsLoggedIn);
+// // let valueInNumber = Number(score)
+// // console.log(typeof valueInNumber);
 
-// console.log(valueInNumber);
+// // console.log(valueInNumber);
+
+
+
+
+//////OPerations/////////////
+
+// let value = 3
+
+
+// let negValue = -value
+// console.log(negValue);
+
+
+// let str1 = "1"
+
+// let str2 = "3"
+
+
+// let str3 = str1 + str2
+
+// console.log(str3);
+
+
+// console.log ( 1 + 2 + "2" +2 + 2);
+
+
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter)
