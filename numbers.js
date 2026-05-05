64dc82cc16dda07e9bfe3d0f48e6729a7b405a0c
Web864 <thelogic266@gmail.com>
2026-05-05 06:57:05 +0000

    operations learned

---
+++ numbers.js
@@ -0,0 +1,25 @@
+
+const balance = new Number (100)
+
+// console.log (balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+
+// const otherNumbers = 23.89238
+
+// console.log(otherNumbers.toPrecision(2))
+// const hundreds = 10000000
+// console.log(hundreds.toLocaleString());
+
+
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10 )+1);
+
+
+const min = 10
+const max = 20 
+
+console.log(Math.floor(Math.random() *(max - min + 1)) + min)
---
+++ string.js
@@ -0,0 +1,20 @@
+const gameName = new String ('its-Mine')
+const accountId = 123678
+
+
+console.log(`my gameName is ${gameName} and accoundid is ${accountId}`)
+
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.toLocaleLowerCase());
+
+const url = "https://hitesh.com/hitesh%20choudary"
+
+
+console.log(url.replace('%20', '-'))
+console.log(url.includes('sunda'))
+console.log(gameName.split('-'))
